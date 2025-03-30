4ac13659ffbc44f981754e0d129f6b2641c17b19
nacho <nacholopez082@gmail.com>
2025-03-30 13:55:02 +0200

    Primer commit - Subiendo el proyecto

---
+++ src/main/java/com/gitt/pat/Practica4Api/Practica4ApiApplication.java
@@ -0,0 +1,13 @@
+package com.gitt.pat.Practica4Api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Practica4ApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Practica4ApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gitt/pat/Practica4Api/Practica4ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gitt.pat.Practica4Api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Practica4ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
